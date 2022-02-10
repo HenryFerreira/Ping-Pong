c63d83bc70b4ddeef282689d9462872eecf34968
HenryFerreira <henryferreira133@gmail.com>
2022-02-10 20:01:11 -0300

    Se crea el Control de teclas presionadas y se agrega el Atributo Speed en el objeto BAR

--- main.js
+++ main.js
@@ -29,14 +29,18 @@
         this.board = board;        
         this.board.bars.push(this);//Acceder al board, a atributo barras y le agrega esta barra
         this.kind = "rectangle";//Dibuja un rectangulo en el BOARD
+        this.speed = 10;//Velocidad de desplazamiento de las barras
     }
     //Metodos del objeto BAR
     self.Bar.prototype = {
         down: function(){
-
+            this.y += this.speed;
         },
         up: function(){
-
+            this.y -= this.speed;
+        },
+        toString: function(){//Mostrar las cordenadas de las barras como un String
+            return "x: " + this.x + " y: " + this.y;
         }
     }
 })();//Fin de la Objeto BAR
@@ -70,21 +74,37 @@
                     break;
                 }
             }
-        }
-		
+        }		
 	}
 })();//Fin de la Objeto BOARD_VIEW
 
+//------------------------------------------------//
+//Variables
+var board = new Board(800,400);
+var bar = new Bar(20,100,40,100,board);
+var bar = new Bar(735,100,40,100,board);
+var canvas = document.getElementById("canvas");
+var board_view = new BoardView(canvas, board);
+//------------------------------------------------//
+
+
+//Controla que tecla fue presionada, en este casp la tecla Up y Down
+document.addEventListener("keydown",function(ev){
+    if(ev.keyCode == 38){
+        bar.up();
+    } else if (ev.keyCode == 40){
+        bar.down();
+    }
+
+    console.log(bar.toString());
+});
 
 
 //Mostrar temporalmente en la ventana
 window.addEventListener("load",main);
 
 function main(){//Función MAIN
-    var board = new Board(800,400);
-    var bar = new Bar(20,100,40,100,board);
-    var canvas = document.getElementById("canvas");
-    var board_view = new BoardView(canvas, board);
+    
     board_view.draw();
 
 };//Cierre de la función MAIN
