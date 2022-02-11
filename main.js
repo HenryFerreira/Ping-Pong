a86894ab173f79aec8bab9ab89afef847725084c
HenryFerreira <henryferreira133@gmail.com>
2022-02-10 21:06:16 -0300

    Crear el metodo de Move para BALL y hacer que la tecla SPACE pause el juego

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-(function(){//Objeto BOARD
-    //Constructor con los Atributos del objeto BOARD/TABLERO
+(function(){//Función BOARD
+    //Constructor con los Atributos de la Función BOARD/TABLERO
     self.Board = function(width, height){//Alto y Ancho
         this.width = width;
         this.height = height;
@@ -9,15 +9,19 @@
         this.ball = null;
     }
 
-    //Metodos del objeto BOARD/TABLERO
+    //Metodos de la Función BOARD/TABLERO
     self.Board.prototype = {
         get elements(){//Devuelve las barras y la pelota que estan en el tablero
-            var elements = this.bars;
+            var elements = this.bars.map(
+                function(bar){
+                    return bar;
+                }
+            );
             elements.push(this.ball);
             return elements;
         }
     }
-})();//Cierre de la Objeto BOARD
+})();//Cierre de la Función BOARD
 
 
 (function(){//Función BALL
@@ -25,17 +29,27 @@
         this.x = x;
         this.y = y;
         this.radius = radius;
-        this.board = board;
         this.speed_y = 0;
         this.speed_x = 3;
+        this.board = board;
+        this.direction = 1;
+
         board.ball = this;
         this.kind = "circle";
-        
     }
+
+    //Metodos de la Función BALL
+    self.Ball.prototype = {
+        move: function(){//Mueve la pelota dentro del canvas
+            this.x += (this.speed_x * this.direction);
+            this.y += (this.speed_y);
+        }
+    }
+
 })();//Fin de la función BALL
 
-(function(){//Objeto BAR - Crea las Barras del juego
-    //Constructor con los Atributos del objeto BAR
+(function(){//Función BAR - Crea las Barras del juego
+    //Constructor con los Atributos de la Función BAR
     self.Bar = function(x, y, width, height, board){// Coordenadas X/Y, Alto, Ancho y Board
         this.x = x;
         this.y = y;
@@ -46,7 +60,8 @@
         this.kind = "rectangle";//Dibuja un rectangulo en el BOARD
         this.speed = 10;//Velocidad de desplazamiento de las barras
     }
-    //Metodos del objeto BAR
+
+    //Metodos de la Función BAR
     self.Bar.prototype = {
         down: function(){
             this.y += this.speed;
@@ -58,10 +73,10 @@
             return "x: " + this.x + " y: " + this.y;
         }
     }
-})();//Fin de la Objeto BAR
+})();//Fin de la Función BAR
 
-(function(){//Objeto BOARD_VIEW - Vista del BOARD/TABLERO
-    //Constructor con los Atributos del objeto BOARD_VIEW
+(function(){//Función BOARD_VIEW - Vista del BOARD/TABLERO
+    //Constructor con los Atributos de la Función BOARD_VIEW
     self.BoardView = function(canvas, board){
         this.canvas = canvas;
         this.canvas.width = board.width//Alto
@@ -70,7 +85,7 @@
         this.ctx = canvas.getContext("2d");
     }
 
-    //Metodos del objeto BOARD_VIEW
+    //Metodos de la Función BOARD_VIEW
     self.BoardView.prototype = {
         clean: function(){//Actualiza el canvas
             this.ctx.clearRect(0, 0, this.board.width, this.board.height);
@@ -82,19 +97,22 @@
 			}
 		},
         play: function(){//Ejecuta los metodos
-            this.clean();
-            this.draw();
+            if(this.board.playing){
+                this.clean();
+                this.draw();
+                this.board.ball.move();
+            }
         }
     }
 
     //Helpers
     function draw(ctx, element) {
             switch (element.kind) {
-                case "rectangle":{
+                case "rectangle":{//Dibuja el RECTANGULO en el canvas
                     ctx.fillRect(element.x, element.y, element.width, element.height);
                     break;
                 }
-                case "circle":{
+                case "circle":{//Dibuja el CIRCULO en el canvas
                     ctx.beginPath();
                     ctx.arc(element.x, element.y, element.radius, 0, 7);
                     ctx.fill();
@@ -103,7 +121,7 @@
                 }
             }
 	}
-})();//Fin de la Objeto BOARD_VIEW
+})();//Fin de la Función BOARD_VIEW
 
 //------------------------------------------------//
 //Variables
@@ -116,31 +134,34 @@ var ball = new Ball(350, 100, 10, board);
 //------------------------------------------------//
 
 //Actualiza los frames de la ventana
-window.requestAnimationFrame(main);
+board_view.draw();
+window.requestAnimationFrame(controller);
 
 //Controla que tecla fue presionada, en este casp la tecla Up y Down
-document.addEventListener("keydown",function(ev){
-    ev.preventDefault();
+document.addEventListener("keydown",function(ev){    
     if(ev.keyCode == 38){//Up
+        ev.preventDefault();
         bar_l.up();
     } else if (ev.keyCode == 40){//Down
+        ev.preventDefault();
         bar_l.down();
     } else if (ev.keyCode == 87){//W
+        ev.preventDefault();
         bar_r.up();
     } else if (ev.keyCode == 83){//S
+        ev.preventDefault();
         bar_r.down();
+    } else if(ev.keyCode == 32){//SPACE
+        ev.preventDefault();
+        board.playing = !board.playing;
     }
     console.log(bar_l.toString());
     console.log(bar_r.toString());
 });
 
-
-//Mostrar temporalmente en la ventana
-//window.addEventListener("load",main);
-
-function main(){//Función MAIN
+function controller(){//Función CONTROLLER
     board_view.play();
     //Actualiza los frames de la ventana
-    window.requestAnimationFrame(main);
+    window.requestAnimationFrame(controller);
 
-};//Cierre de la función MAIN
+};//Cierre de la función CONTROLLER
